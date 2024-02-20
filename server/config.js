2f5cc9acc9f68b095788b60433c3deecd5ac32c4
Aryaman <aryamanvashisht900@gmail.com>
2024-02-20 15:21:29 +0530

    updated gitignore

---
+++ server/config.js
@@ -0,0 +1,6 @@
+export default {
+    JWT_SECRET: "Wdb3SUw28zza97eHV9Mu3NkBDiXcAoHAbIip/ezNGi4=",
+    EMAIL:"manuvashisht9@gmail.com",
+    PASSWORD:"lvwooqdajcflqsce",
+    ATLAS_URI:"mongodb+srv://admin:admin123@cluster0.uer6rdj.mongodb.net/?retryWrites=true&w=majority"
+}
